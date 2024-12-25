75e5345d05def6e2b7e9afed23f620f2d5d1b985
Pedro Gritter <pedro12345g@gmail.com>
2024-12-25 07:21:23 +0000

    feat: :sparkles: add dummy api service

--- app/dashboard/@products/page.tsx
+++ app/dashboard/@products/page.tsx
@@ -1,7 +1,19 @@
+import { getProducts } from "@/services/product";
 import React from "react";
 
-const Products = () => {
-  return <div>Display Products Here</div>;
+const Products = async () => {
+  const productsJson = await getProducts();
+  console.log(productsJson);
+
+  return (
+    <div>
+      <div className="flex flex-col gap-3 justify-center">
+        {productsJson.products.map((product) => (
+          <p key={product.id}>{product.title}</p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Products;
--- services/product.ts
+++ services/product.ts
@@ -1,5 +1,10 @@
 // TODO: Export product query function
 // Get data from the API - https://dummyjson.com/docs/products
 export async function getProducts(): Promise<unknown[]> {
-  return [];
+  const res = await fetch("https://dummyjson.com/products");
+
+  if (!res.ok) {
+    throw new Error("Error fetching products");
+  }
+  return res.json();
 }
