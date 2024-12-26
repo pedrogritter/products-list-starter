dacc8b134b2cfd01b27bb7bc48c425ab91c98629
Pedro Gritter <pedro12345g@gmail.com>
2024-12-26 15:28:52 +0000

    feat: :zap: Adds cache memoization to the API service

--- services/product.ts
+++ services/product.ts
@@ -1,19 +1,36 @@
-// TODO: Export product query function
-// Get data from the API - https://dummyjson.com/docs/products
-export async function getProducts(): Promise<unknown[]> {
-  const res = await fetch("https://dummyjson.com/products");
+"server only";
+import { memoize } from "nextjs-better-unstable-cache";
 
-  if (!res.ok) {
-    throw new Error("Error fetching products");
+export const getProducts = memoize(
+  async (): Promise<unknown[]> => {
+    const res = await fetch("https://dummyjson.com/products");
+
+    if (!res.ok) {
+      throw new Error("Error fetching products");
+    }
+    return res.json();
+  },
+  {
+    persist: true,
+    revalidateTags: () => ["products"],
+    suppressWarnings: true,
+    logid: "products",
   }
-  return res.json();
-}
+);
 
-export async function getProduct(id: number): Promise<unknown[]> {
-  const res = await fetch(`https://dummyjson.com/products/${id}`);
+export const getProduct = memoize(
+  async (id: number): Promise<unknown[]> => {
+    const res = await fetch(`https://dummyjson.com/products/${id}`);
 
-  if (!res.ok) {
-    throw new Error("Error fetching product");
+    if (!res.ok) {
+      throw new Error("Error fetching product");
+    }
+    return res.json();
+  },
+  {
+    persist: true,
+    revalidateTags: () => ["product_info"],
+    suppressWarnings: true,
+    logid: "product_info",
   }
-  return res.json();
-}
+);
